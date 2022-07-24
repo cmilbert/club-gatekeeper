87acdef81f44dc92bd75f899de6ea2034e55dab2
Chris Milbert <chrismilbert@gmail.com>
2022-07-24 18:59:25 -0400

    Moving spidev dependency

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo sed -i "s/#dtparam=spi=on/dtparam=spi=on/g" /boot/config.txt
 # install apt dependencies
 sudo apt update
 sudo apt install -y python3 python3-pip python3-dev \
-  espeak alsa-utils spidev
+  espeak alsa-utils
 
 # install python dependencies
 pip3 install pipx
