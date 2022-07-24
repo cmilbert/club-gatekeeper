2127e5edbfee64543467ba082eb21ced418a02bc
Chris Milbert <chrismilbert@gmail.com>
2022-07-24 18:27:18 -0400

    Updating install script to enable SPI and reboot

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# Enable SPI if it is not enabled
+echo "Enabling SPI interface for RFID reader"
+sudo sed -i "s/#dtparam=spi=on/dtparam=spi=on/g" /boot/config.txt
+
 # install apt dependencies
 sudo apt update
 sudo apt install python3 python3-pip espeak alsa-utils -y
 
 # install python dependencies
 pip3 install pyttsx3
+
+echo "Rebooting"
+sudo reboot 0
