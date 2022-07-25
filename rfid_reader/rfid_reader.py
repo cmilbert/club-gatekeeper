f48a9d47947ccdc905c0f97db86dd3827f559104
Chris Milbert <chrismilbert@gmail.com>
2022-07-24 20:34:43 -0400

    Adding TTS, a writer, and dropping poetry in favor of requirements.txt

--- rfid_reader/rfid_reader.py
+++ rfid_reader/rfid_reader.py
@@ -2,7 +2,9 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 import simpleaudio as sa
-
+import pyttsx3
+ 
+tts_engine = pyttsx3.init()
 reader = SimpleMFRC522()
 
 def play_beep():
@@ -10,11 +12,18 @@ def play_beep():
     play_obj = wave_obj.play()
     play_obj.wait_done()
 
+def say_welcome(name: str):
+    tts_engine.say(f"Welcome {name}!")
+    tts_engine.runAndWait()
+
 try:
     while True:
+        print("Please scan badge")
         id, text = reader.read()
         print(id)
         print(text)
         play_beep()
+        say_welcome(text)
+
 finally:
     GPIO.cleanup()
