b330d4861951e3be19edb8565a4e11b8bfe7bd72
Chris Milbert <chrismilbert@gmail.com>
2022-07-24 20:23:25 -0400

    Switching to simpleaudio for pi compatibility

--- rfid_reader/rfid_reader.py
+++ rfid_reader/rfid_reader.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-from playsound import playsound
+import simpleaudio as sa
 
 reader = SimpleMFRC522()
+
+def play_beep():
+    wave_obj = sa.WaveObject.from_wave_file("../assets/audio/beep.wav")
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
 
 try:
     while True:
         id, text = reader.read()
         print(id)
         print(text)
-        playsound('../assets/audio/beep.wav')
+        play_beep()
 finally:
     GPIO.cleanup()
