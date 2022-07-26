942980b3c8cd6dfc6ebc8d8fb98a147547a16442
DrgnFireYellow <ethan@milbert.dev>
2022-07-26 18:52:12 -0400

    Work on db

--- member-management/add_member.py
+++ member-management/add_member.py
@@ -1,7 +1,8 @@
 import sqlite3
 import sys
+from unicodedata import name
 
 con = sqlite3.connect(sys.argv[0] + "/../members.db")
 cur = con.cursor()
 # @cmilbert: please store data and id from a scanned rfid tag in variables here.
-cur.execute(f"INSERT INTO members()")
+cur.execute(f"INSERT INTO members({id}, {name})")
